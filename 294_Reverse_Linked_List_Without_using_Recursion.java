aa05f513588ab4eecb0ba1e9688dcd6500d8e44a
manjeet <manjeetsingh188500@gmail.com>
2024-11-19 20:51:01 +0530

    Add new program

---
+++ 294_Reverse_Linked_List_Without_using_Recursion.java
@@ -0,0 +1,43 @@
+/*Question reverse Linked List:
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ * 
+ * 
+ */
+
+
+public class 294_Reverse_Linked_List_Without_using_Recursion {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev=new ListNode();
+        prev=null;
+        ListNode curr=head;
+
+        while(curr!=null){
+           ListNode next=new ListNode();
+           next=curr.next;
+           curr.next=prev;
+           prev=curr;
+           curr=next;
+        }
+
+        return prev;
+       
+   }
+}
