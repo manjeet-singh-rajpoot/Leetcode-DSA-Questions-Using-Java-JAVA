0c161376e56e7e542fc37677021dc8896ca6da90
manjeet <manjeetsingh188500@gmail.com>
2025-02-27 14:05:37 +0530

    Add new program

---
+++ 362_single_Number.java
@@ -0,0 +1,52 @@
+/*
+ * Question : 
+ *   Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+
+ 
+
+Constraints:
+
+1 <= nums.length <= 3 * 104
+-3 * 104 <= nums[i] <= 3 * 104
+Each element in the array appears twice except for one element which appears only once.
+
+ */
+
+import java.util.*;
+public class 362_single_Number {
+    static int solve(int arr[]){
+       int result=0;
+
+       for(int num:arr){
+         result=result^num; 
+       } 
+    return result;
+  }
+    
+    public static void main(String[] args) {
+        int nums[]={2,2,1};
+        System.out.print(solve(nums));
+    }
+}
