12e40822d1d5014cc5eb94a04314bb7150dead4c
manjeet <manjeetsingh188500@gmail.com>
2025-04-04 00:28:03 +0530

    Add new program

---
+++ 414_generate_Parenthesis.java
@@ -0,0 +1,65 @@
+
+/*
+ * Question : 
+ *    
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+ 
+
+Constraints:
+
+1 <= n <= 8
+Seen this question in a real interview before?
+1/5
+Yes
+No
+Accepted
+2.3M
+Submissions
+3M
+Acceptance Rate
+76.7%
+Topics
+Companies
+ * 
+ */
+public class 414_generate_Parenthesis {
+    public List<String> generateParenthesis(int n) {
+            List<String>ans=new ArrayList<>();
+            dfs(0,0,"",n,ans);
+
+   return ans;
+
+    }
+  private void dfs(int openP,int closeP,String s,int n,List<String>res){
+
+     if(openP==closeP && openP + closeP == n * 2){
+           res.add(s);
+           return ;
+     }
+
+     if(openP < n){
+          dfs(openP+1,closeP,s + "(",n,res);
+     }
+
+     if(closeP < openP){
+          dfs(openP,closeP+1,s + ")",n,res);
+     }
+
+  }
+}
+  public static void main(String[] args) {
+      int n=3;
+      System.out.print(generateParenthesis(n));
+  }    
+
