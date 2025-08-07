09c7345021f7025a2e58743589d4376fdefa9f92
manjeet <manjeetsingh188500@gmail.com>
2025-08-07 09:34:59 +0530

    Add new program

---
+++ 462_Pascal_Triangle_2D.java
@@ -0,0 +1,20 @@
+public class 462_Pascal_Triangle_2D {
+     public List<List<Integer>> generate(int n) {
+          List<List<Integer>> ans=new ArrayList<>();
+          if(n==0){
+             return ans;
+          }
+         ans.add(new ArrayList<>());
+         ans.get(0).add(1);
+         for(int i=1;i<n;i++){
+            List<Integer>curr=new ArrayList<>();
+            curr.add(1);
+            for(int j=1;j<i;j++){
+                curr.add(ans.get(i-1).get(j-1) + ans.get(i-1).get(j));
+            }
+          curr.add(1);
+          ans.add(curr);
+         }
+  return ans;
+    }
+}
