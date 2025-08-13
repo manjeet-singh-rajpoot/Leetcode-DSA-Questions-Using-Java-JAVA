2ee7866b2826cb0d293c830356a90e8996ef85ab
manjeet <manjeetsingh188500@gmail.com>
2025-08-13 13:37:10 +0530

    Add new program

---
+++ 467_String_Matching_in_An_Array.java
@@ -0,0 +1,16 @@
+public class 467_String_Matching_in_An_Array {
+     public List<String> stringMatching(String[] words) {
+            List<String>ans=new ArrayList<>();
+            int n=words.length;
+            for(int i=0;i<n;i++){
+                 for(int j=0;j<n;j++){
+                     if(i != j && words[j].contains(words[i])){
+                        ans.add(words[i]);
+                        break;
+                     }
+                 }
+            }
+
+        return ans;
+    }
+}
