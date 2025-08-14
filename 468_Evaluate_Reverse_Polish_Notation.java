f84bdcbefd7286ac50a852447d4e0bb648b694dc
manjeet <manjeetsingh188500@gmail.com>
2025-08-14 11:05:16 +0530

    Add new program

---
+++ 468_Evaluate_Reverse_Polish_Notation.java
@@ -0,0 +1,27 @@
+public class  468_Evaluate_Reverse_Polish_Notation{
+public int evalRPN(String[] tokens) {
+         Stack<Integer>st=new Stack<>();
+         for(String c:tokens){
+              if(c.equals("+")){
+                 st.push(st.pop() + st.pop());
+              }else if(c.equals("-")){
+                   int s=st.pop();
+                   int f=st.pop();
+                   st.push(f-s);
+              }else if(c.equals("*")){
+                 int s=st.pop();
+                 int f=st.pop();
+                 st.push(f * s);
+              }else if(c.equals("/")){
+                int s=st.pop();
+                int f=st.pop();
+                st.push(f / s);
+              }else{
+                st.push(Integer.parseInt(c));
+              }
+         }
+
+         return st.peek();
+    }
+    
+}
